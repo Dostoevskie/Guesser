ab2ef5847ce5472d730e27c83c2a76bb6817be1c
Dostoevskie <37383557+Dostoevskie@users.noreply.github.com>
2021-02-17 15:45:51 +0100

    Update Program.cs

--- Homework 2 - Guesser/Program.cs
+++ Homework 2 - Guesser/Program.cs
@@ -9,8 +9,6 @@ namespace guesser
 {
     class Guesser
     {
-        // the "const" keyword makes the value of the variable final, meaning it can never be modified
-        // a variable with "const" acts also as if it had the "static" modifier, meaning it can be accessed without needing to instantiate an object
 
         const string wordToGuess = "Giraffe";
 
